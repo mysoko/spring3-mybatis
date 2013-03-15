c7884c04c634aec42a80d65d77a8e54008e7af2f
mysoko <liyunheng@gmail.com>
2013-03-15 14:05:48 +0800

    Update AcctinfoMapper.java

--- src/main/java/com/mining/app/persistence/AcctinfoMapper.java
+++ src/main/java/com/mining/app/persistence/AcctinfoMapper.java
@@ -2,6 +2,10 @@ package com.mining.app.persistence;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Select;
 
 import com.mining.app.domain.Acctinfo;
@@ -12,18 +16,21 @@ public interface AcctinfoMapper {
 	
 	Acctinfo getAcctinfoById(Acctinfo acctinfo);
 
+	// @Insert("insert into Acctinfo (acct_no, operators, total_amount) values (#{acctNo}, #{operators}, #{totalAmount})")
+	// @Options(useGeneratedKeys = true, keyProperty = "id")
 	int insertAcctinfo(Acctinfo acctinfo);
 
 	void updateAcctinfo(Acctinfo acctinfo);
 	
-	@Select("SELECT id, acct_no as acctNo, operators, total_amount as totalAmount FROM Acctinfo")
+	@Select("select id, acct_no as acctNo, operators, total_amount as totalAmount from Acctinfo")
 	List<Acctinfo> getAcctinfoList();
 	
-	// @Select("SELECT id, acct_no AS acctNo, operators, total_amount AS totalAmount FROM Acctinfo WHERE operators=#{operators}")
-	@Select("SELECT * FROM Acctinfo WHERE operators=#{operators}")
+	// @Select("select id, acct_no as acctNo, operators, total_amount as totalAmount from Acctinfo where operators=#{operators}")
+	@Select("select * from Acctinfo where operators=#{operators}")
 	@Results({
 		@Result(property = "acctNo", column = "acct_no"),
 		@Result(property = "totalAmount", column = "total_amount")
 	})
 	List<Acctinfo> getAcctinfoByOperators(Acctinfo acctinfo);
+	
 }
