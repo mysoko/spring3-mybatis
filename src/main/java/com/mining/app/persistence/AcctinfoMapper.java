4c78831dd49004b406cc4510301943f98b661f31
mysoko <liyunheng@gmail.com>
2013-03-15 13:51:48 +0800

    Update AcctinfoMapper.java

--- src/main/java/com/mining/app/persistence/AcctinfoMapper.java
+++ src/main/java/com/mining/app/persistence/AcctinfoMapper.java
@@ -19,4 +19,11 @@ public interface AcctinfoMapper {
 	@Select("SELECT id, acct_no as acctNo, operators, total_amount as totalAmount FROM Acctinfo")
 	List<Acctinfo> getAcctinfoList();
 	
+	// @Select("SELECT id, acct_no AS acctNo, operators, total_amount AS totalAmount FROM Acctinfo WHERE operators=#{operators}")
+	@Select("SELECT * FROM Acctinfo WHERE operators=#{operators}")
+	@Results({
+		@Result(property = "acctNo", column = "acct_no"),
+		@Result(property = "totalAmount", column = "total_amount")
+	})
+	List<Acctinfo> getAcctinfoByOperators(Acctinfo acctinfo);
 }
